bf9bc5ce1918a8e864e3a1730bfea3d2961e8d9f
Pedro Arlyx <pedroarlyx7@gmail.com>
2025-09-21 09:32:13 -0300

    Atividade valendo 1 ponto

--- atividade1p/src/App.tsx
+++ atividade1p/src/App.tsx
@@ -5,10 +5,12 @@ import Contador from "./components/Contador";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <Navbar />
-      <main className="flex-1 flex flex-col items-center justify-center gap-16 p-8">
-        <div className="flex flex-col md:flex-row gap-16 w-full max-w-5xl items-start justify-center">
+    <div className="min-h-screen bg-gradient-to-r from-blue-700 to-purple-700 flex flex-col">
+      <div className="flex justify-center w-full">
+        <div className="w-full max-w-5xl"><Navbar /></div>
+      </div>
+      <main className="flex-1 flex flex-col items-center justify-center gap-16 w-full max-w-5xl mx-auto p-8">
+        <div className="flex flex-col md:flex-row gap-16 items-center justify-center w-full">
           <Contador />
           <Card />
         </div>
--- atividade1p/src/components/Navbar.tsx
+++ atividade1p/src/components/Navbar.tsx
@@ -1,14 +1,12 @@
 export default function Navbar() {
     return (
-        <nav className="bg-blue-600 text-white px-8 py-4 flex justify-between items-center shadow-md w-full">
-           <h1 className="text-2xl font-bold ">Atividade</h1>
-            <div className="flex-1 flex justify-end">
-                <ul className="flex gap-8 text-lg list-none m-0 p-0">
-                    <li className="hover:text-gray-200 cursor-pointer">Home -</li>
-                    <li className="hover:text-gray-200 cursor-pointer">Sobre -</li>
-                    <li className="hover:text-gray-200 cursor-pointer">Contador</li>
-                </ul>
-            </div>
+        <nav className="bg-blue-600 text-white px-8 py-4 flex justify-between items-center shadow-md w-full ">
+            <h1 className="text-3xl font-extrabold tracking-tight">Atividade</h1>
+            <ul className="flex gap-8 text-lg list-none m-0 p-0">
+                <li className="hover:text-gray-200 cursor-pointer transition-colors">Home</li>
+                <li className="hover:text-gray-200 cursor-pointer transition-colors">Sobre</li>
+                <li className="hover:text-gray-200 cursor-pointer transition-colors">Contador</li>
+            </ul>
         </nav>
     );
 }
